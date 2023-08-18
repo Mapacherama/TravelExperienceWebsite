d4d2c1acf6aacfd6e208f4a11262d9a4bf6d7d50
Jerome Tesselaar <mapacherama@gmail.com>
2023-08-18 12:31:55 +0200

    Feature: Images working in the hero section

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  declarations: [HomepageComponent],
+  imports: [
+    SharedModule,
+    RouterModule.forChild(routes)
+  ],
   exports: [RouterModule]
 })
 export class HomeRoutingModule { }
--- src/app/home/homepage/homepage.component.ts
+++ src/app/home/homepage/homepage.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from 'src/app/services/photo.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  images: any[] | undefined;
+  responsiveOptions: any[] | undefined;
+
+  constructor(private photoService: PhotoService) {}
+
+  ngOnInit() {
+    this.images = this.photoService.getImages();
+    this.responsiveOptions = [
+        {
+            breakpoint: '1024px',
+            numVisible: 5
+        },
+        {
+            breakpoint: '768px',
+            numVisible: 3
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 1
+        }
+    ];
+}
 
 }
