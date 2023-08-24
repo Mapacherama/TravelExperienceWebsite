db781ae09d37111c39a3292303e8e91b55caf598
Jerome Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2023-08-24 11:34:10 +0200

    Feature: Edited a photo, and implemented a new hompage picture

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -11,7 +11,7 @@ export class PhotoService {
   getImages(): any[] {
     // For now, we'll return a mock promise. You can replace this with an actual HTTP request or other logic.
     return [
-      { source: '../../assets/images/4HiQCeKePLm3bOknwe2HdaIbvbd4VSNtPoBO1ibvhiI-1537x2048.jpg', alt: 'Description 1', title: 'Title 1' },
+      { source: '../../assets/images/Brandenburger_Tor_Selfie_With_Text.png', alt: 'Description 1', title: 'Title 1' },
       { source: '../../assets/images/MzDDFx6PsYJKP8KZl69Hqfe6Rsbu0Ktqig7_m1qPtjI-1537x2048.jpg', alt: 'Description 2', title: 'Title 2' },
       // ... more images
     ];
